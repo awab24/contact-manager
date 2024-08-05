003d211898a8d7bcf930e0d2cb33ee0f9adf8b26
Awab Hassan <awab1hassan1@gmail.com>
2024-08-05 20:55:01 +0300

    dd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
